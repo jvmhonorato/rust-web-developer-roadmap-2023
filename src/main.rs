c369feeb03a0e4091c1910b76faf9dcb0fa9b8e0
vituhonorato <vituhonorato.89@gmail.com>
2023-07-25 20:05:44 -0300

    start Rust Course

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+#![allow(unused)]
+
+use std::io;
+use rand::Rng;
+use std::io::{Write, BufReader, BufRead, ErrorKind};
+use std::fs::File;
+use std::cmp::Ordering;
+
 fn main() {
     println!("Hello, world!");
 }
+ 
